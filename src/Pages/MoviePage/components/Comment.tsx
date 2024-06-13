abb03d4ccfbd0310c987cd641fe95f89fbf72183
adammetal <adam.metal@codecool.com>
2024-06-13 11:12:29 +0200

    add skeleton for comment

---
+++ src/Pages/MoviePage/components/Comment.tsx
@@ -0,0 +1,19 @@
+export default function Comment() {
+  return (
+    <article className="comment">
+      <header className="author">
+        <h1>Metal Adam</h1>
+      </header>
+      <p className="body">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet quo magnam
+        quaerat doloribus non itaque ipsum commodi. Molestiae quidem est nobis
+        itaque veritatis expedita repellat, maxime tenetur culpa fugit numquam
+        cumque labore, commodi officiis suscipit nulla. Aliquam sit deleniti
+        maxime.
+      </p>
+      <div className="rating">
+        6 / 10
+      </div>
+    </article>
+  );
+}
